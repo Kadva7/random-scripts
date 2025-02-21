97f1caf93e9e23d8949433237667a489166a5fe1
Kadva7 <83033242+Kadva7@users.noreply.github.com>
2025-02-21 19:36:07 +0100

    add option to open the directory using xdg-open

--- bash/dumuni.sh
+++ bash/dumuni.sh
@@ -3,10 +3,11 @@
 
 # main directory const
 munidir="/home/kadva7/Documents/muni/"
+FMAN=($(xdg-mime query default inode/directory | sed 's/.desktop//'))
 
 # possible arguments:
-#   > $1 - none | subject code
-#   > $2 - none | "du" | "files" | "f"
+#   > $1 - none | subject code | "open"
+#   > $2 - none | "du" | "files" | "f" | "open" | o
 
 ll >/dev/null || alias ll='ls -lav --ignore=..'
 
@@ -15,18 +16,23 @@ year_now="$(date +%Y)"
 maindir="${munidir}${subject}/"
 
 if [ -d "$maindir" ]; then
-    cd "$maindir"
     if [ "$2" = "du" ]; then
         if [ -d "${maindir}du" ]; then
-            cd "du"
+            cd "${maindir}du"
         elif [ -d "${maindir}du${year_now}" ]; then
-            cd "du${year_now}"
+            cd "${maindir}du${year_now}"
         else
             echo "Cannot find homework folder!"
         fi
     elif [ "$2" = "f" ] || [ "$2" = "files" ]; then
-        ls -ahil "${maindir}"
+        ls -ahil "$maindir"
+    elif [ "$2" = "o" ] || [ "$2" = "open" ]; then
+        retGarbage=($($FMAN "$maindir" -- 2>&- 2<&-)) & disown
+    else
+        cd "${maindir}"
     fi
+elif [ "$1" = "open" ]; then
+    retGarbage=($($FMAN "$munidir" -- 2>&- 2<&-)) & disown
 else
     echo "Cannot find directory for specified subject!"
 fi
