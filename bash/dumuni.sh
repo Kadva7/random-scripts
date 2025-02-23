cc79e3bbc15ca471368035d947900fabbacee573
Kadva7 <83033242+Kadva7@users.noreply.github.com>
2025-02-23 23:19:37 +0100

    add option to detect and open vscode workspaces

--- bash/dumuni.sh
+++ bash/dumuni.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-
 # main directory const
 function _muni_get_directory() {
     [ -f '~/.config/muni-script' ] && echo "$(cat "~/.config/muni-script")" && return 0
@@ -13,10 +12,11 @@ function _muni_get_directory() {
 
 munidir="$(_muni_get_directory)"
 FMAN=($(xdg-mime query default inode/directory | sed 's/.desktop//'))
+VSCODE_EDITOR_EXEC=vscodium
 
 # possible arguments:
 #   > $1 - none | subject code | "open"
-#   > $2 - none | "du" | "files" | "f" | "open" | o
+#   > $2 - none | "du" | "files" | "f" | "open" | o | "vscode"
 
 [ "$(alias ll 2>&-)" ] || alias ll='ls -lav --ignore=..'
 
@@ -24,29 +24,54 @@ subject="$1"
 year_now="$(date +%Y)"
 maindir="${munidir}${subject}/"
 
-if [ -d "$maindir" ]; then
-    if [ "$2" = "du" ]; then
-        if [ -d "${maindir}du" ]; then
+if [[ -d "$maindir" ]]; then
+    if [[ "$2" = du ]]; then
+        if [[ -d "${maindir}du" ]]; then
             cd "${maindir}du"
-        elif [ -d "${maindir}du${year_now}" ]; then
+        elif [[ -d "${maindir}du${year_now}" ]]; then
             cd "${maindir}du${year_now}"
         else
             echo "Cannot find homework folder!"
         fi
-    elif [ "$2" = "f" ] || [ "$2" = "files" ]; then
+    elif [[ "$2" = f ]] || [[ "$2" = files ]]; then
         ls -ahil "$maindir"
-    elif [ "$2" = "o" ] || [ "$2" = "open" ]; then
-        retGarbage=($($FMAN "$maindir" -- 2>&- 2<&-)) & disown
-    elif [ "$2" = "l" ] || [ "$2" = "list" ]; then
+    elif [[ "$2" = o ]] || [[ "$2" = open ]]; then
+        r=$($FMAN "$maindir" -- 2>&- 2<&-) & disown
+    elif [[ "$2" = l ]] || [[ "$2" = list ]]; then
         ll "$maindir"
+    elif [[ "$2" = vscode ]]; then
+        # EDITOR
+        editor=vscodium
+        [[ "$(vscodium --help 2>&-)" ]] || (echo "No VS Codium detected! Please set editor to what you use instead of it!" && return 1)
+
+        workspaces=($(cd "${maindir}" && find -P -xdev -name "*.code-workspace"))
+        if [[ ${#workspaces[@]} -eq 0 ]]; then
+            echo "No VS Code workspaces found in subject directory!"
+        elif [[ ${#workspaces[@]} -eq 1 ]]; then
+            cd "${maindir}" && r=$(vscodium "${workspaces[0]}") & disown
+        else
+            echo "Multiple workspaces found!"
+            local i=0
+            for elem in $workspaces; do
+                echo "[$i] $elem"
+                i=$((i + 1))
+            done
+            while (read -p "Enter a number between 0 and $((i - 1))" num); do
+                if ! [[ $num == +([0-9]) ]]; then
+                    echo "Input must be a non-negative integer!"
+                elif [[ $num -lt 0 ]] || [[ $num -gt $((i - 1)) ]]; then
+                    echo "Number must be between the possible value!"
+                else
+                    r=$(vscodium ${workspaces[num]} 2>&-) & disown
+                    return 0
+                fi
+            done
+        fi
     else
         cd "${maindir}"
     fi
 elif [ "$1" = "open" ]; then
-    retGarbage=($($FMAN "$munidir" -- 2>&- 2<&-)) & disown
+    r=$($FMAN "$munidir" -- 2>&- 2<&-) & disown
 else
-    echo "Cannot find directory for specified subject!"
+    echo -e "Cannot find directory for specified subject!"
 fi
-
-
-
