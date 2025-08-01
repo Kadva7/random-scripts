3c4091eb1239c363d762ee822db42b205a942078
Kadva7 <83033242+Kadva7@users.noreply.github.com>
2025-08-02 01:09:29 +0200

    cleanup: better naming for variables, also use sed for upcoming aliases

--- bash/config.sh
+++ bash/config.sh
@@ -4,26 +4,27 @@
 # A bash script for the config file, so that my stupid scripts don't just
 # do stuff that is kinda unknown
 ###
+set -e
 
-SCHOOL_CONFIG_DIR=$([ -z "$SCHOOL_CONFIG_DIR" ] && echo "$HOME/.config/school-bash" || echo "$SCHOOL_CONFIG_DIR")
+SCH_CONF_DIR=$([ -z "$SCH_CONF_DIR" ] && echo "$HOME/.config/school-bash" || echo "$SCH_CONF_DIR")
+SCH_CONF_FILE=$([ -z "$SCH_CONF_FILE" ] && echo "muni-script" || echo "$SCH_CONF_FILE" )
 
-SUBJECTS="$( [ -n "$SUBJECTS" ] && echo "$SUBJECTS" || echo "$SCHOOL_CONFIG_DIR/muni-script")"
+SUBJ_LOC="$( [ -n "$SUBJ_LOC" ] && echo "$SUBJ_LOC" || echo $(sed -n '1p' "$SCH_CONF_DIR/$SCH_CONF_FILE") )"
 
-[ -s $SUBJECTS ] && echo "$(cat -- "$SUBJECTS")" && exit 0
+[ -s "$SUBJ_LOC" ] && echo "$SUBJ_LOC" && exit 0
 
-while [ ! -s $SUBJECTS ] && read -p "Please type where the directory containing the subjects is (directory of them): " line
+while [ ! -s $SUBJ_LOC ] && read -p "Please type where the directory containing the subjects is (directory of them): " line
 do
     if [[ -d $line ]]; then
         echo "Do you wish to set \'$line\' as the base directory? (y/N)"
         read inp
         if [[ $inp = y ]] || [[ $inp = Y ]]; then
-            mkdir -p "$SCHOOL_CONFIG_DIR"
-            echo "$line" > "$SCHOOL_CONFIG_DIR/script"
+            mkdir -p "$SCH_CONF_DIR"
+            echo "$line" > "$SCH_CONF_DIR/muni-script"
             break
         fi
     else
         echo "\'$line\' is not a directory!"
     fi
-
 done
 
